e9eb8a3fcb23c8a1799a8d2feddf6490054f4a0f
Angelina <angel.lina.den@gmail.com>
2021-05-13 11:54:22 +0300

    add tests for websocket using pytest

--- taxi_app/routing.py
+++ taxi_app/routing.py
@@ -1,3 +1,8 @@
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import path
 
-application = ProtocolTypeRouter({})
+from trips.consumers import TaxiConsumer
+
+application = ProtocolTypeRouter({
+    'websocket': URLRouter([path('taxi/', TaxiConsumer.as_asgi()),]),
+})
---
+++ trips/tests/test_websocket.py
@@ -0,0 +1,34 @@
+from channels.testing import WebsocketCommunicator
+import pytest
+
+from taxi_app.routing import application
+
+
+TEST_CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+    },
+}
+
+
+@pytest.mark.asyncio
+class TestWebSocket:
+    async def test_can_connect_to_server(self, settings):
+        settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
+        communicator = WebsocketCommunicator(application=application, path='/taxi/')
+        connected, _ = await communicator.connect()
+        assert connected is True
+        await communicator.disconnect()
+
+    async def test_can_send_and_receive_massages(self, settings):
+        settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
+        communicator = WebsocketCommunicator(application=application, path='/taxi/')
+        message = {
+            'type': 'echo.message',
+            'data': 'This is a test message.'
+        }
+        connected, _ = await communicator.connect()
+        await communicator.send_json_to(message)
+        response = await communicator.receive_json_from()
+        assert response == message
+        await communicator.disconnect()
